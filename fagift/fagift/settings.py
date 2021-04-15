16b4867400a9c9e4f371b7bb592e9fe3b4838879
Kyubum Jang <jkb2221@gmail.com>
2021-04-15 23:48:47 +0900

    fix: django setting

--- fagift/fagift/settings.py
+++ fagift/fagift/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3ziq!1qn4b=dgcc!6*#p(!%0zmjeg+63&j(jcb5tcgz#cc@)f_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
