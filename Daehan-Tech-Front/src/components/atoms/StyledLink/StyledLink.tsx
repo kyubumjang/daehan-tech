46cf1bce9494d8403a57f77f89ed8d8183a67b5d
Kyubum Jang <jkb2221@gmail.com>
2022-07-13 17:19:20 +0900

    feat(StyledLink): Add StyledLink Components

---
+++ Daehan-Tech-Front/src/components/atoms/StyledLink/StyledLink.tsx
@@ -0,0 +1,23 @@
+import { Link, LinkProps } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+const StyledRouterLink = styled(Link)`
+  text-decoration: none;
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+`;
+
+interface IProps extends LinkProps {
+  children: JSX.Element | string;
+}
+
+function StyledLink({ children, ...props }: IProps) {
+  return <StyledRouterLink {...props}>{children}</StyledRouterLink>;
+}
+
+export default StyledLink;
